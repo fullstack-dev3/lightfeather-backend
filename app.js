86fe292508979efdd35f963fd63609946573491b
full-stack-david <davidchan12323@gmail.com>
2022-09-16 03:22:44 -0500

    Add API to get Supervisors

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
 const express = require('express');
+const https = require('https');
 const app = express();
 const port = 3000;
+
+app.get('/api/supervisors', (req, res) => {
+  const url = 'https://o3m5qixdng.execute-api.us-east-1.amazonaws.com/api/managers';
+
+  https.get(url, response => {
+    let body = [];
+
+    response.on('data', chunk => {
+      body.push(chunk);
+    });
+
+    response.on('end', () => {
+      let result = JSON.parse(Buffer.concat(body).toString());
+      
+      result.sort((a, b) => {
+        if (a.lastName.toUpperCase() === b.lastName.toUpperCase()) {
+          return a.firstName > b.firstName ? 1 : -1;
+        }
+
+        if (a.jurisdiction === b.jurisdiction) {
+          return a.lastName > b.lastName ? 1 : -1;
+        }
+
+        return a.jurisdiction > b.jurisdiction ? 1 : -1;
+      });
+
+      res.send(result);
+    });
+  });
+});
 
 app.listen(port, () => {
   
